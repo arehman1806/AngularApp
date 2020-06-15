cc857938b8889ed02d6dc9a80ad073e67fd92569
arehman1806 <arehmannasir@outlook.com>
2020-06-15 10:26:31 +0100

    Data Binding

--- src/app/shared/Dish.ts
+++ src/app/shared/Dish.ts
@@ -1,4 +1,7 @@
+import { Comment } from './Comment';
+
 export class Dish {
+
   id: string;
   name: string;
   image: string;
@@ -7,4 +10,5 @@ export class Dish {
   label: string;
   price: string;
   description: string;
+  comments: Comment[];
 }
