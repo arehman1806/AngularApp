46c108fa1c0e1e4a8234e52cbe11a484f827e784
arehman1806 <arehmannasir@outlook.com>
2020-06-17 13:01:29 +0100

    Single Page Applications Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Dish} from '../shared/Dish';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import {DishService} from '../services/dish.service';
 
 
 @Component({
@@ -10,12 +13,19 @@ import {Dish} from '../shared/Dish';
 
 export class DishdetailComponent implements OnInit {
 
-  @Input()
   dish: Dish;
 
-  constructor() { }
+  constructor(private dishservice: DishService,
+              private route: ActivatedRoute,
+              private location: Location) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
